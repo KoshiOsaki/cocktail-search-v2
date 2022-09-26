339475f1af9f4db2e4619b97f4f0d6943558ad0a
KoshiOsaki <osaki36dg@gmail.com>
2022-09-26 09:21:36 +0900

    localhost切り替えの追加

--- src/components/pages/Index/index.tsx
+++ src/components/pages/Index/index.tsx
@@ -1,92 +1,63 @@
 import { Autocomplete, Box, IconButton, Radio, Tab, Tabs, TextField, Typography } from '@mui/material';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { MdAddCircleOutline, MdSearch } from 'react-icons/md';
-import { db } from '../../../firebase/init';
-import { Cocktail, cocktailFromDoc, includeMaterialFromDoc, Material } from '../../../types/cocktail';
+import { useCocktails } from '../../../hooks/useCocktails';
 import { Layout } from '../../uiParts/Layout';
 import { DefaultCocktailTab } from './DefaultCocktailTab';
 import { OriginalCocktailTab } from './OriginalCocktailTab';
 
 export const IndexPage = () => {
+  const { cocktailList } = useCocktails();
   const mydata: any = [];
   const [data, setData] = useState();
   const [findName, setFindName] = useState('');
   const [findMa, setFindMa] = useState('');
-  const [cocktailList, setCocktailList] = useState<Cocktail[]>([]);
-
-  const fetchIncludeMaterial = async (cocktailId: string) => {
-    const _materialList: Material[] = [];
-    const materialCollection = collection(db, 'cocktails', cocktailId, 'materials');
-    const materialQuery = query(materialCollection);
-    const querySnapshot = await getDocs(materialQuery);
-    querySnapshot.forEach((doc) => {
-      const _material = includeMaterialFromDoc(doc);
-      _materialList.push(_material);
-    });
-    return _materialList;
-  };
-
-  //rubyの時も
-  useEffect(() => {
-    (async () => {
-      const _cocktailList: Cocktail[] = [];
-      const cocktailCollection = collection(db, 'cocktails');
-      const cocktailQuery = query(cocktailCollection);
-      const querySnapshot = await getDocs(cocktailQuery);
-      querySnapshot.forEach((doc) => {
-        const _cocktail = cocktailFromDoc(doc);
-        fetchIncludeMaterial(_cocktail.id).then((materialList) => {
-          _cocktail.material.push(...materialList);
-        });
-        _cocktailList.push(_cocktail);
-      });
-      console.log(_cocktailList);
-      setCocktailList(_cocktailList);
-    })();
-  }, []);
 
   const [value, setValue] = useState(0);
-  const [selectedValue, setSelectedValue] = useState('a');
+  const [selectedSearchItem, setSelectedSearchItem] = useState(0);
 
-  const handleSearchItem = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedValue(event.target.value);
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchItem = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedSearchItem(Number(e.target.value));
   };
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
+  const onClickSearch = () => {
+    if (searchInput.length > 0) {
+    } else {
+    }
+  };
+
   return (
     <Layout>
       <div className="flex ">
         <div className="flex items-center">
-          <Radio checked={selectedValue === 'a'} onChange={handleSearchItem} value="a" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} />
+          <Radio checked={selectedSearchItem == 0} onChange={handleSearchItem} value={0} name="radio-buttons" />
           <Typography>カクテル名</Typography>
         </div>
         <div className="flex items-center">
-          <Radio checked={selectedValue === 'b'} onChange={handleSearchItem} value="b" name="radio-buttons" inputProps={{ 'aria-label': 'A' }} />
+          <Radio checked={selectedSearchItem == 1} onChange={handleSearchItem} value={1} name="radio-buttons" />
           <Typography>材料名</Typography>
         </div>
       </div>
       <div className="flex">
         <Autocomplete
+          inputValue={searchInput}
+          onInputChange={(event: any, newValue: string) => {
+            setSearchInput(newValue);
+          }}
           freeSolo
           id="free-solo-2-demo"
           disableClearable
           options={['123', '222']}
           sx={{ width: '300px' }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              InputProps={{
-                ...params.InputProps,
-                type: 'search',
-              }}
-            />
-          )}
+          renderInput={(params) => <TextField {...params} label="" />}
         />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={onClickSearch}>
           <MdSearch />
         </IconButton>
       </div>
---
+++ src/types/apiType.ts
@@ -0,0 +1,4 @@
+export enum ApiType {
+  FIREBASE = 'firebase',
+  LOCALHOST = 'localhost',
+}
